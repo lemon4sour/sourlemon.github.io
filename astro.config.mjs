6fcda63dae341206487ba67ff5a21ed945b4e188
lemon4sour <lemon4sour@gmail.com>
2026-05-09 03:02:49 +0300

    learned astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://lemon4sour.github.io"
+  site: "https://lemon4sour.github.io",
+  integrations: [preact()]
 });
